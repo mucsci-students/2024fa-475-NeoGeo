635e952422326de0aca4d03bc0e53a55aae6ace8
NStrick <nickstricker99@gmail.com>
2024-10-12 14:05:08 -0400

    effects, health gUI

--- Project/Assets/Weapons/Weapon.cs
+++ Project/Assets/Weapons/Weapon.cs
@@ -8,24 +8,28 @@ public class Weapon : MonoBehaviour
     //public for testing
     public Rigidbody2D wpnBody;
     public CompositeCollider2D wpnCollider;
-    public SpriteRenderer wpnSprite;
-    public int damage = 1;
-    public int weight = 1;
+    public Sprite wpnSprite;
+    public float damage = 1.0f;
+    public float weight = 1.0f;
     public bool isProjectile = false;
+
+    private SpriteRenderer render;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        wpnBody = new Rigidbody2D();
-        wpnCollider = new CompositeCollider2D();
-        wpnSprite = GetComponent<SpriteRenderer>();
+        wpnBody = GetComponent<Rigidbody2D>();
+        wpnCollider = GetComponent<CompositeCollider2D>();
+        wpnSprite = GetComponent<Sprite>();
+        render = GetComponent<SpriteRenderer>();
+        render.sprite = wpnSprite;
     }
 
     void OnCollisionEnter2D(Collision2D col)
     {
         // Try to get the Health component of the object hit
-        Health targetHealth = col.gameObject.GetComponent<Health>();
+        PlayerStats targetHealth = col.gameObject.GetComponent<PlayerStats>();
         
         // If the object has a Health component and is alive, deal damage
         if (targetHealth != null)
